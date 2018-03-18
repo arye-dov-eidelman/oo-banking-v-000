3808ea5f43ce370f94896aa310de2575890714bc
Arye Dov Eidelman <aryedov.e@gmail.com>
2018-03-18 16:36:54 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,7 @@ class Transfer
   end
 
   def execute_transaction
-    valid? ? nil : return "Transaction rejected. Please check your account balance."
+    valid? ? : return "Transaction rejected. Please check your account balance."
     return if @status == "complete"
     @sender.balance -= @amount
     @receiver.balance += @amount
